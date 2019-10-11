4d27d3133f4cf8035ee5b5d2fb9b400620ff3d42
RainVisitor <abc873693@gmail.com>
2019-10-11 11:30:10 +0800

    Revert "Add preference support for flutter web "
    This reverts commit 08714a75954e4d372e0aeda9a7212a37c4ba7867.

--- lib/utils/preferences.dart
+++ lib/utils/preferences.dart
@@ -1,4 +1,3 @@
-import 'dart:html' as web;
 import 'dart:io';
 
 import 'package:encrypt/encrypt.dart';
@@ -17,11 +16,8 @@ class Preferences {
 
   static SharedPreferences prefs;
 
-  static web.Storage _localStorage;
-
   static init() async {
     if (kIsWeb) {
-      _localStorage = web.window.localStorage;
     } else if (Platform.isIOS || Platform.isAndroid) {
       prefs = await SharedPreferences.getInstance();
       PackageInfo packageInfo = await PackageInfo.fromPlatform();
@@ -36,19 +32,11 @@ class Preferences {
   }
 
   static Future<Null> setStringSecurity(String key, String data) async {
-    if (kIsWeb)
-      _localStorage[key] = encrypter.encrypt(data, iv: Constants.iv).base64;
-    else
-      await prefs?.setString(
-          key, encrypter.encrypt(data, iv: Constants.iv).base64);
+    await prefs?.setString(key, encrypter.encrypt(data, iv: Constants.iv).base64);
   }
 
   static String getStringSecurity(String key, String defaultValue) {
-    String data = '';
-    if (kIsWeb)
-      data = _localStorage[key] ?? '';
-    else
-      data = prefs?.getString(key) ?? '';
+    String data = prefs?.getString(key) ?? '';
     if (data == '')
       return defaultValue;
     else
@@ -56,83 +44,35 @@ class Preferences {
   }
 
   static Future<Null> setString(String key, String data) async {
-    if (kIsWeb)
-      _localStorage[key] = data;
-    else
-      await prefs?.setString(key, data);
+    await prefs?.setString(key, data);
   }
 
   static String getString(String key, String defaultValue) {
-    if (kIsWeb)
-      return (_localStorage[key]) ?? defaultValue;
-    else
-      return prefs?.getString(key) ?? defaultValue;
+    return prefs?.getString(key) ?? defaultValue;
   }
 
   static Future<Null> setInt(String key, int data) async {
-    if (kIsWeb)
-      _localStorage[key] = data.toString();
-    else
-      await prefs?.setInt(key, data);
+    await prefs?.setInt(key, data);
   }
 
   static int getInt(String key, int defaultValue) {
-    if (kIsWeb) {
-      int value;
-      try {
-        value = int.parse(_localStorage[key]);
-      } catch (e) {
-        value = defaultValue;
-      }
-      return value ?? defaultValue;
-    } else
-      return prefs?.getInt(key) ?? defaultValue;
+    return prefs?.getInt(key) ?? defaultValue;
   }
 
   static Future<Null> setDouble(String key, double data) async {
-    if (kIsWeb)
-      _localStorage[key] = data.toString();
-    else
-      await prefs?.setDouble(key, data);
+    await prefs?.setDouble(key, data);
   }
 
   static double getDouble(String key, double defaultValue) {
-    if (kIsWeb) {
-      double value;
-      try {
-        value = double.parse(_localStorage[key]);
-      } catch (e) {
-        value = defaultValue;
-      }
-      return value ?? defaultValue;
-    }
     return prefs?.getDouble(key) ?? defaultValue;
   }
 
   static Future<Null> setBool(String key, bool data) async {
-    if (kIsWeb)
-      _localStorage[key] = data.toString();
-    else
-      await prefs?.setBool(key, data);
+    await prefs?.setBool(key, data);
   }
 
   static bool getBool(String key, bool defaultValue) {
-    if (kIsWeb) {
-      bool value;
-      switch (_localStorage[key]) {
-        case 'true':
-          value = true;
-          break;
-        case 'false':
-          value = false;
-          break;
-        default:
-          value = defaultValue;
-          break;
-      }
-      return value ?? defaultValue;
-    } else
-      return prefs?.getBool(key) ?? defaultValue;
+    return prefs?.getBool(key) ?? defaultValue;
   }
 
   static Future<Null> setStringList(String key, List<String> data) async {
