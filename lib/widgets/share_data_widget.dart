ed4649e2ade0db8da58dfd5c5f0b0bad39a245a5
RainVisitor <abc873693@gmail.com>
2020-02-15 04:37:49 +0800

    Update InheritedWidget setting by flutter v1.12

--- lib/widgets/share_data_widget.dart
+++ lib/widgets/share_data_widget.dart
@@ -9,7 +9,7 @@ class ShareDataWidget extends InheritedWidget {
   ShareDataWidget({this.data, Widget child}) : super(child: child);
 
   static ShareDataWidget of(BuildContext context) {
-    return context.inheritFromWidgetOfExactType(ShareDataWidget);
+    return context.dependOnInheritedWidgetOfExactType();
   }
 
   @override
