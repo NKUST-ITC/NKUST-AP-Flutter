fb895316763ff578c0b187f908bcf449c93109e9
RainVisitor <abc873693@gmail.com>
2020-05-17 18:10:44 +0800

    Update to v3.3.15

--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -275,7 +275,7 @@ class SettingPageState extends State<SettingPage> {
       packageInfo = await PackageInfo.fromPlatform();
     setState(() {
       isOffline = Preferences.getBool(Constants.PREF_IS_OFFLINE_LOGIN, false);
-      appVersion = packageInfo?.version ?? '3.3.13';
+      appVersion = packageInfo?.version ?? '3.3.15';
       courseNotify = Preferences.getBool(Constants.PREF_COURSE_NOTIFY, false);
       displayPicture =
           Preferences.getBool(Constants.PREF_DISPLAY_PICTURE, true);
