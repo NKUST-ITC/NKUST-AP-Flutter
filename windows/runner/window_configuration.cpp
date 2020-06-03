970f8a453c5f82a7576b89c0d37f380b117929df
Rainvisitor <abc873693@gmail.com>
2020-06-04 00:51:57 +0800

    Update windows app setting

--- windows/runner/window_configuration.cpp
+++ windows/runner/window_configuration.cpp
@@ -1,6 +1,6 @@
 #include "window_configuration.h"
 
-const wchar_t* kFlutterWindowTitle = L"nkust_ap";
+const wchar_t* kFlutterWindowTitle = L"NKUST AP";
 const unsigned int kFlutterWindowOriginX = 10;
 const unsigned int kFlutterWindowOriginY = 10;
 const unsigned int kFlutterWindowWidth = 1280;
