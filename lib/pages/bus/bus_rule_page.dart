d167128f753581c0471780e0d93e89e5a8090ecd
junklin <ade0955013094@gmail.com>
2020-06-03 15:48:37 +0800

    bus_rule_english_version

--- lib/pages/bus/bus_rule_page.dart
+++ lib/pages/bus/bus_rule_page.dart
@@ -45,14 +45,14 @@ class BusRulePageState extends State<BusRulePage> {
                   color: ApTheme.of(context).grey, height: 1.3, fontSize: 16.0),
               children: [
                 TextSpan(
-                  text: '預約校車\n',
+                  text: app.busTitle,
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 24.0,
                   ),
                 ),
                 TextSpan(
-                  text: "• 請上 ",
+                  text: app.please,
                 ),
                 TextSpan(
                     text: "http://bus.kuas.edu.tw/",
@@ -64,83 +64,71 @@ class BusRulePageState extends State<BusRulePage> {
                       ..onTap =
                           () => ApUtils.launchUrl('http://bus.kuas.edu.tw/')),
                 TextSpan(
-                  text: " 校車預約系統預約校車\n" +
-                      "• 可預約14天以內的校車班次\n" +
-                      "• 為配合總務處派車需求預約時間\n",
+                  text: app.fourteen_day
                 ),
                 TextSpan(
-                  text: '■ 9點以前的班次：請於發車前15個小時預約\n'
-                      '■ 9點以後的班次：請於發車前5個小時預約\n',
+                  text: app.reservation_time,
                   style: TextStyle(color: ApTheme.of(context).red),
                 ),
                 TextSpan(
-                  text: '• 取消預約時間\n',
+                  text: app.cancelling_title,
                 ),
                 TextSpan(
-                  text: '■ 9點以前的班次：請於發車前15個小時預約\n'
-                      '■ 9點以後的班次：請於發車前5個小時預約\n',
+                  text: app.canceling_time,
                   style: TextStyle(color: ApTheme.of(context).red),
                 ),
                 TextSpan(
-                  text: "• 請全校師生及職員依規定預約校車，若因未預約校車而無法到課或上班者，請自行負責\n",
+                  text: app.busRuleFollow,
                 ),
                 TextSpan(
-                  text: '上車\n',
+                  text: app.take_on,
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 24.0,
                   ),
                 ),
                 TextSpan(
-                  text: "• 每次上車繳款20元",
+                  text: app.twenty_dollars,
                 ),
                 TextSpan(
-                  text: '（未發卡前先以投幣繳費，請自備20元銅板投幣）\n',
+                  text: app.prepare_coins,
                   style: TextStyle(color: ApTheme.of(context).blueText),
                 ),
                 TextSpan(
-                  text: "• 請持學生證或教職員證(未發卡前先採用身分證識別)上車\n",
+                  text: app.id_card,
                 ),
                 TextSpan(
-                  text: '• 未攜帶證件者請排後補區\n',
+                  text: app.no_id_card,
                   style: TextStyle(
                       color: ApTheme.of(context).red,
                       fontWeight: FontWeight.bold),
                 ),
                 TextSpan(
-                  text:
-                      "• 請依預約的班次時間搭乘(例如：8:20與9:30視為不同班次），未依規定者不得上車，並計違規點數一點\n",
+                  text: app.following_time,
                   style: TextStyle(color: ApTheme.of(context).red),
                 ),
                 TextSpan(
-                  text: "• 逾時或未預約搭乘者請至候補車道排隊候補上車。\n" +
-                      "候補上車\n" +
-                      "• 在正常車道上車時未通過驗證者(ex.未預約該班次)，請改至候補車道排隊候補上車。\n" +
-                      "• 候補者需等待預約該班次的人全部上車之後才依序遞補上車\n",
+                  text: app.late_and_no_reservation,
                 ),
                 TextSpan(
-                  text: '候補上車\n',
+                  text: app.standby_title,
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 24.0,
                   ),
                 ),
                 TextSpan(
-                  text: "• 未依預約的班次搭乘者，視為違規，計違規點數一次(例如：8:20與9:30視為不同班次）\n" +
-                      "• 因教師臨時請假、臨時調課致使需提前或延後搭車，得向開課系所提出申請，並由系所之交通車系統管理者註銷違規紀錄。\n",
+                  text: app.standby_rule,
                 ),
                 TextSpan(
-                  text: '罰款\n',
+                  text: app.fine_title,
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 24.0,
                   ),
                 ),
                 TextSpan(
-                  text: "• 違規罰款金額計算，違規前三次不計點，從第四次開始違規記點，每點應繳納等同車資之罰款\n" +
-                      "• 違規點數統計至學期末為止(上學期學期末1/31，下學期8/31)，新學期違規點數重新計算。當學期罰款未繳清者，次學期停止預約權限至罰款繳清為止\n" +
-                      "• 罰款請自行列印違規明細後至自動繳費機或總務處出納組繳費，繳費後憑收據至總務處事務組銷帳(當天開列之收據須於當天銷帳)，銷帳完後隔天凌晨4點後才可預約當天9點後的校車。\n" +
-                      "• 罰款點數如有疑義，請於違規發生日起10日內(含星期例假日)逕向總務處事務組確認。\n",
+                  text: app.fine_rule,
                   style: TextStyle(color: ApTheme.of(context).red),
                 ),
               ]),
