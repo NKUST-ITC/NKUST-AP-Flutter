01b748f1d8144360011dfd9489f4d1649c54d658
RainVisitor <abc873693@gmail.com>
2020-08-06 13:48:46 +0800

    Update windows files by flutter v1.20.0 create

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -4,8 +4,8 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <file_chooser_plugin.h>
-#include <url_launcher_plugin.h>
+#include <file_chooser/file_chooser_plugin.h>
+#include <url_launcher_fde/url_launcher_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FileChooserPluginRegisterWithRegistrar(
