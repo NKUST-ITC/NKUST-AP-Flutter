e4ada53ee0e4d6b97245f57c2a242c2ade8077ce
takidog <wow@taki.dog>
2020-08-16 03:22:40 +0800

    Add leave submit method on leave api

--- lib/api/leave_helper.dart
+++ lib/api/leave_helper.dart
@@ -6,11 +6,17 @@ import 'package:cookie_jar/cookie_jar.dart';
 import 'package:nkust_ap/api/private_cookie_manager.dart';
 //Config
 import 'package:nkust_ap/config/constants.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:http_parser/http_parser.dart';
+
 //parser
 import 'package:nkust_ap/api/parser/leave_parser.dart';
 //model
 import 'package:nkust_ap/models/leave_data.dart';
 import 'package:nkust_ap/models/leave_submit_info_data.dart';
+import 'package:nkust_ap/models/leave_submit_data.dart';
+import 'package:intl/intl.dart';
+import 'package:html/parser.dart' show parse;
 
 import 'helper.dart';
 
@@ -169,5 +175,140 @@ class LeaveHelper {
           contentType: Headers.formUrlEncodedContentType),
     );
     return LeaveSubmitInfoData.fromJson(leaveSubmitInfoParser(res.data));
+  }
+
+  Future<Response> leavesSubmit(LeaveSubmitData data,
+      {PickedFile proofImage}) async {
+    //force relogin to aviod error.
+    await leaveLogin();
+
+    Response res = await dio.get(
+      "http://leave.nkust.edu.tw/CK001MainM.aspx",
+    );
+
+    var requestData = hiddenInputGet(res.data);
+    requestData[r"ctl00$ContentPlaceHolder1$CK001$ButtonEnter"] = "進入請假作業";
+
+    res = await dio.post(
+      "http://leave.nkust.edu.tw/CK001MainM.aspx",
+      data: requestData,
+      options: Options(
+          followRedirects: false,
+          contentType: Headers.formUrlEncodedContentType),
+    );
+
+    requestData = hiddenInputGet(res.data);
+    var dateFormate = DateFormat("yyyy/MM/dd");
+    var beginDate = dateFormate.parse(data.days[0].day);
+    var endDate = dateFormate.parse(data.days[data.days.length - 1].day);
+
+    requestData[r"ctl00$ContentPlaceHolder1$CK001$DateUCCBegin$text1"] =
+        "${beginDate.year - 1911}/${beginDate.month}/${beginDate.day}";
+    ;
+    requestData[r"ctl00$ContentPlaceHolder1$CK001$DateUCCEnd$text1"] =
+        "${endDate.year - 1911}/${endDate.month}/${endDate.day}";
+    ;
+    requestData[r"ctl00$ContentPlaceHolder1$CK001$ButtonCommit"] = "下一步";
+    res = await dio.post(
+      "http://leave.nkust.edu.tw/CK001MainM.aspx",
+      data: requestData,
+      options: Options(
+          followRedirects: false,
+          contentType: Headers.formUrlEncodedContentType),
+    );
+    if (res.data.toString().indexOf("alert(") > -1) {
+      return null;
+    }
+    var submitData = leaveSubmitInfoParser(res.data.toString());
+    print("on submit main page.");
+    print("Change leave type");
+    Map<String, dynamic> globalRequestData = {};
+
+    globalRequestData[r"ctl00$ContentPlaceHolder1$CK001$TextBoxReason"] =
+        data.reasonText;
+    globalRequestData[r"ctl00$ContentPlaceHolder1$CK001$ddlTeach"] =
+        data.teacherId;
+    globalRequestData[
+            r"ctl00$ContentPlaceHolder1$CK001$RadioButtonListOption"] =
+        data.leaveTypeId;
+    if (data.delayReasonText != null &&
+        res.data.toString().indexOf("延遲理由") > -1) {
+      globalRequestData[r"ctl00$ContentPlaceHolder1$CK001$TextBoxDelayReason"] =
+          data.delayReasonText;
+    }
+    var document = parse(res.data.toString());
+
+    print("generate need click button list");
+    var trObj =
+        document.getElementsByClassName("mGrid")[0].getElementsByTagName("tr");
+    if (trObj.length < 2) {
+      print("Error: not found leave days options");
+      return null;
+    }
+    List<String> _clickList = [];
+    for (int i = 1; i < trObj.length; i++) {
+      var td = trObj[i].getElementsByTagName("td");
+      var _leaveDays = data.days[i - 1].dayClass;
+      for (int l = 0; l < _leaveDays.length; l++) {
+        _clickList.add(td[submitData["timeCodes"].indexOf(_leaveDays[l]) + 3]
+            .getElementsByTagName("input")[0]
+            .attributes["name"]);
+      }
+    }
+    print("click leave class");
+
+    for (int i = 0; i < _clickList.length; i++) {
+      var requestData = hiddenInputGet(res.data.toString());
+      requestData.addAll(globalRequestData);
+
+      requestData[_clickList[i]] = "";
+      res = await dio.post(
+        "http://leave.nkust.edu.tw/CK001MainM.aspx",
+        data: requestData,
+        options: Options(
+            followRedirects: false,
+            contentType: Headers.formUrlEncodedContentType),
+      );
+      //click covid-19 alert.
+
+    }
+    requestData = hiddenInputGet(res.data.toString());
+    requestData.addAll(globalRequestData);
+    if (res.data.toString().indexOf("ContentPlaceHolder1_CK001_cbFlag") > -1) {
+      requestData[r"ctl00$ContentPlaceHolder1$CK001$cbFlag"] = "on";
+    }
+    requestData[r'ctl00$ContentPlaceHolder1$CK001$ButtonCommit2'] = '下一步';
+    res = await dio.post(
+      "http://leave.nkust.edu.tw/CK001MainM.aspx",
+      data: requestData,
+      options: Options(
+          followRedirects: false,
+          contentType: Headers.formUrlEncodedContentType),
+    );
+
+    print("End submit page");
+    print("Submit and add leave proof image.");
+    requestData = hiddenInputGet(res.data.toString());
+    requestData[r"ctl00$ContentPlaceHolder1$CK001$ButtonSend"] = '存檔';
+    if (proofImage != null) {
+      print("Add proof image");
+      requestData[r'ctl00$ContentPlaceHolder1$CK001$FileUpload1'] =
+          await MultipartFile.fromFile(proofImage.path,
+              filename: "proof_image.jpg",
+              contentType: MediaType.parse("image/jpeg"));
+    }
+
+    FormData formData = FormData.fromMap(requestData);
+
+    dio.options.headers["Content-Type"] =
+        "multipart/form-data; boundary=${formData.boundary}";
+    res = await dio.post("http://leave.nkust.edu.tw/CK001MainM.aspx",
+        data: formData);
+
+    if (res.data.toString().indexOf("假單存檔成功") > -1) {
+      return res;
+    }
+
+    return null;
   }
 }
