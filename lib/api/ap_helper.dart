2fc386299b82aeb9ba4fa8863ff4bb7ef144da37
takidog <wow@taki.dog>
2020-08-25 15:55:15 +0800

    Add bytes response option on apQuery

--- lib/api/ap_helper.dart
+++ lib/api/ap_helper.dart
@@ -137,6 +137,7 @@ class WebApHelper {
     Map<String, String> queryData, {
     String cacheKey,
     Duration cacheExpiredTime,
+    bool bytesResponse,
   }) async {
     /*
     Retrun type Response <Dio>
@@ -155,25 +156,44 @@ class WebApHelper {
     dynamic requestData;
     if (cacheKey == null) {
       _options = Options(contentType: Headers.formUrlEncodedContentType);
+      if (bytesResponse != null) {
+        _options.responseType = ResponseType.bytes;
+      }
       requestData = queryData;
     } else {
       dio.options.headers["Content-Type"] = "application/x-www-form-urlencoded";
-      _options = buildCacheOptions(
-        cacheExpiredTime ?? Duration(seconds: 60),
+      Options other_options;
+      if (bytesResponse != null) {
+        other_options = Options(responseType: ResponseType.bytes);
+      }
+      _options = buildConfigurableCacheOptions(
+        options: other_options,
+        maxAge: cacheExpiredTime ?? Duration(seconds: 60),
         primaryKey: cacheKey,
       );
       requestData = formUrlEncoded(queryData);
     }
-    Response request = await dio.post(
-      url,
-      data: requestData,
-      options: _options,
-    );
+    Response<dynamic> request;
+
+    if (bytesResponse != null) {
+      request = await dio.post<List<int>>(
+        url,
+        data: requestData,
+        options: _options,
+      );
+    } else {
+      request = await dio.post(
+        url,
+        data: requestData,
+        options: _options,
+      );
+    }
+
     if (apLoginParser(request.data) == 2) {
       if (Helper.isSupportCacheData) _manager.delete(cacheKey);
       reLoginReTryCounts += 1;
       await apLogin(username: Helper.username, password: Helper.password);
-      return apQuery(queryQid, queryData);
+      return apQuery(queryQid, queryData, bytesResponse: bytesResponse);
     }
     reLoginReTryCounts = 0;
     return request;
