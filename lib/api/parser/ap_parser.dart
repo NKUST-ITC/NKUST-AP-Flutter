c50940b2ddcafc220b14cf0036e4f4705c99453e
takidog <wow@taki.dog>
2020-08-25 15:58:51 +0800

    Add Trans-Encoding bytes parser

--- lib/api/parser/ap_parser.dart
+++ lib/api/parser/ap_parser.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'dart:io';
+import 'dart:typed_data';
 import 'package:html/parser.dart' show parse;
 
 String clearHtml(String html) {
@@ -10,7 +12,43 @@ String clearHtml(String html) {
   return temp.replaceAll(new RegExp(r"\r\n(\w{1,4})\r\n"), "");
 }
 
-int apLoginParser(String html) {
+String clearTransEncoding(List<int> htmlBytes) {
+  // htmlBytes is fixed-length list, need copy.
+  var tempData = new List<int>.from(htmlBytes);
+
+  //Add /r/n on first word.
+  tempData.insert(0, 10);
+  tempData.insert(0, 13);
+
+  int startIndex = 0;
+  for (int i = 0; i < tempData.length - 1; i++) {
+    //check i and i+1 is /r/n
+    if (tempData[i] == 13 && tempData[i + 1] == 10) {
+      if (i - startIndex - 2 <= 4 && i - startIndex - 2 > 0) {
+        //check in this range word is number or A~F (Hex)
+        int removeCount = 0;
+        for (int _strIndex = startIndex + 2; _strIndex < i; _strIndex++) {
+          if ((tempData[_strIndex] > 47 && tempData[_strIndex] < 58) ||
+              (tempData[_strIndex] > 64 && tempData[_strIndex] < 71) ||
+              (tempData[_strIndex] > 96 && tempData[_strIndex] < 103)) {
+            removeCount++;
+          }
+        }
+        if (removeCount == i - startIndex - 2) {
+          tempData.removeRange(startIndex, i + 2);
+        }
+        //Subtract offset
+        i -= i - startIndex - 2;
+        startIndex -= i - startIndex - 2;
+      }
+      startIndex = i;
+    }
+  }
+
+  return utf8.decode(tempData, allowMalformed: true);
+}
+
+int apLoginParser(dynamic html) {
   /*
     Retrun type Int
     0 : Login Success
