662eb07fd9122271842a9ab9d7bc401d9dde626b
RainVisitor <abc873693@gmail.com>
2020-10-04 18:08:23 +0800

    Update widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,5 +3,13 @@
 // provides. For example, you can send tap and scroll gestures. You can also use WidgetTester to
 // find child widgets in the widget tree, read text, and verify that the values of widget properties
 // are correct.
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nkust_ap/app.dart';
 
-void main() {}
+void main() {
+  testWidgets('smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+  });
+}
