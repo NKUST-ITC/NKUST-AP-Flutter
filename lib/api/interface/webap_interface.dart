0ef270235c110a719ee43812f12d144051bfdd90
RainVisitor <abc873693@gmail.com>
2020-11-04 13:07:27 +0800

    Add web ap interface

---
+++ lib/api/interface/webap_interface.dart
@@ -0,0 +1,25 @@
+import 'package:ap_common/models/semester_data.dart';
+import 'package:ap_common/models/user_info.dart';
+import 'package:ap_common/models/score_data.dart';
+import 'package:ap_common/models/course_data.dart';
+import 'package:nkust_ap/models/midterm_alerts_data.dart';
+import 'package:nkust_ap/models/room_data.dart';
+
+abstract class WebApInterface {
+  Future<void> logout();
+
+  Future<UserInfo> getUserInfo();
+
+  Future<SemesterData> getSemesters();
+
+  Future<ScoreData> getScores({String year, String value});
+
+  Future<CourseData> getCourseTable({String year, String value});
+
+  Future<MidtermAlertsData> getMidtermAlerts({String year, String value});
+
+  Future<RoomData> roomList({String campusAreaId});
+
+  Future<CourseData> getRoomCourseTable(
+      {String roomId, String year, String value});
+}
