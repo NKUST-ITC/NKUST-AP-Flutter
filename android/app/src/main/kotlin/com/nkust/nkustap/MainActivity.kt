b1fbc9d83680bd310ca622563848acef5c49bd48
RainVisitor <abc873693@gmail.com>
2021-01-03 23:31:35 +0800

    Support android 11 transparent gesture navigation bar

--- android/app/src/main/kotlin/com/nkust/nkustap/MainActivity.kt
+++ android/app/src/main/kotlin/com/nkust/nkustap/MainActivity.kt
@@ -1,6 +1,15 @@
 package com.nkust.ap.flutter
 
+import android.os.Build
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+        }
+    }
 }
