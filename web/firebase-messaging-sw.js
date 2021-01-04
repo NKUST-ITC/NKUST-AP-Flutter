41faa9464a15c89b8f4b510cb35bf8cab22b51dc
RainVisitor <abc873693@gmail.com>
2021-01-04 11:19:37 +0800

    Add firebase cloud message setting for flutter web

---
+++ web/firebase-messaging-sw.js
@@ -0,0 +1,20 @@
+importScripts("https://www.gstatic.com/firebasejs/8.0.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.0.1/firebase-messaging.js");
+
+firebase.initializeApp( {
+    apiKey: "AIzaSyBpHUWczANDziA-Gzxoh_ybGnEZq9JmKL4",
+    authDomain: "nkust-ap-flutter.firebaseapp.com",
+    databaseURL: "https://nkust-ap-flutter.firebaseio.com",
+    projectId: "nkust-ap-flutter",
+    storageBucket: "nkust-ap-flutter.appspot.com",
+    messagingSenderId: "141403473068",
+    appId: "1:141403473068:web:2804dce650446efef34b09",
+    measurementId: "G-TSD51EKRLX"
+});
+
+const messaging = firebase.messaging();
+
+// Optional:
+messaging.onBackgroundMessage((m) => {
+  console.log("onBackgroundMessage", m);
+});
