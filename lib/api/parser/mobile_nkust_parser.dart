9e59eac3d8bab29b77ac3692153a17a464aca9ab
RainVisitor <abc873693@gmail.com>
2021-02-02 14:42:38 +0800

    Move mobile nkust parser to single file

---
+++ lib/api/parser/mobile_nkust_parser.dart
@@ -0,0 +1,292 @@
+import 'dart:convert';
+
+import 'package:ap_common/models/course_data.dart';
+import 'package:ap_common/models/score_data.dart';
+import 'package:ap_common/models/user_info.dart';
+import 'package:ap_common/utils/ap_localizations.dart';
+import 'package:html/parser.dart' as html;
+import 'package:nkust_ap/models/midterm_alerts_data.dart';
+
+class MobileNkustParser {
+  static List<Map<String, dynamic>> busViolationRecords(
+    String rawHtml, {
+    bool paidStatus,
+  }) {
+    final document = html.parse(rawHtml);
+    List<Map<String, dynamic>> result = [];
+    var format = DateFormat('yyyy/MM/dd HH:mm');
+
+    for (var trElement in document.getElementsByTagName('tr')) {
+      Map<String, dynamic> _temp = {};
+
+      var tdElements = trElement.getElementsByTagName('td');
+      var timeElement = tdElements[1].getElementsByTagName('div')[0];
+      _temp['isPayment'] = paidStatus;
+      var startAndGoal = tdElements[3].text.split(" 到 ");
+      _temp['startStation'] = startAndGoal[0];
+      _temp['endStation'] = startAndGoal[1];
+      _temp['amountend'] = int.parse(tdElements[4].text);
+      _temp['homeCharteredBus'] = false;
+
+      _temp['time'] = format.parse(
+          '${timeElement.text.substring(0, 10)} ${timeElement.text.substring(14)}');
+      result.add(_temp);
+    }
+    return result;
+  }
+
+  static List<Map<String, dynamic>> busUserRecords(
+    String rawHtml, {
+    String startStation,
+    String endStation,
+  }) {
+    final document = html.parse(rawHtml);
+    List<Map<String, dynamic>> result = [];
+    var format = DateFormat('yyyy/MM/dd HH:mm');
+
+    for (var trElement in document.getElementsByTagName('tr')) {
+      Map<String, dynamic> _temp = {};
+      _temp['cancelKey'] =
+          trElement.getElementsByTagName("input")[0].attributes['value'];
+      var tdElements = trElement.getElementsByTagName('td');
+
+      _temp['dateTime'] = format.parse(
+          '${tdElements[1].text.substring(0, 10)} ${tdElements[1].text.substring(14)}');
+      _temp['state'] = "";
+      _temp['travelState'] = "";
+      _temp['start'] = startStation;
+      _temp['end'] = endStation;
+      result.add(_temp);
+    }
+
+    return result;
+  }
+
+  static CourseData courseTable(rawHtml) {
+    final document = html.parse(rawHtml);
+
+    Map<String, dynamic> result = {
+      "courses": [],
+      'timeCodes': [],
+    };
+    var inputElements = document.getElementsByTagName("input");
+    var coursesJson = jsonDecode(inputElements[0].attributes['value']);
+    var periodTimeJson = jsonDecode(inputElements[1].attributes['value']);
+
+    periodTimeJson.forEach((periodTime) {
+      result["timeCodes"].add({
+        "title": "第${periodTime["PeriodName"]}節",
+        "startTime":
+            "${periodTime["BegTime"].substring(0, 2)}:${periodTime["BegTime"].substring(2, 4)}",
+        "endTime":
+            "${periodTime["EndTime"].substring(0, 2)}:${periodTime["EndTime"].substring(2, 4)}",
+      });
+    });
+
+    coursesJson.forEach((course) {
+      var _temp = {
+        "code": course['SelectCode'].toString(),
+        "title": course['CourseName'].toString(),
+        "className": course['ClassNameAbr'].toString(),
+        "group": course['CourseGroup'].toString(),
+        "units": course['Credit'].toString(),
+        "hours": course['Hour'].toString(),
+        "required": course['OptionName'].toString(),
+        "at": course['Annual'],
+        "sectionTimes": [],
+        "instructors": course['TeacherName'].split(","),
+        "location": {
+          "building": "",
+          "room": course['CourseRoom'] ?? "",
+        }
+      };
+      for (var time in course['CourseWeekPeriod']) {
+        _temp['sectionTimes'].add({
+          "weekday": int.parse(time['CourseWeek']),
+          "index": int.parse(time['CoursePeriod'])
+        });
+      }
+      result["courses"].add(_temp);
+    });
+
+    return CourseData.fromJson(result);
+  }
+
+  static MidtermAlertsData midtermAlerts(rawHtml) {
+    final midtermAlertsData = MidtermAlertsData();
+    return midtermAlertsData;
+  }
+
+  static List<Map<String, dynamic>> busTimeTable(
+    rawHtml, {
+    String time,
+    String startStation,
+    String endStation,
+  }) {
+    var document = html.parse(rawHtml);
+
+    List<Map<String, dynamic>> result = [];
+
+    for (var trElement in document.getElementsByTagName('tr').sublist(1)) {
+      Map<String, dynamic> _temp = {};
+
+      // Element can't get ById. so build new parser object.
+      var _input_document = html.parse(trElement.outerHtml);
+      _temp['canBook'] = true;
+
+      if (_input_document.getElementById('ReserveEnable').attributes['value'] ==
+          null) {
+        //can't book.
+        _temp['canBook'] = false;
+      }
+      _temp['busId'] =
+          _input_document.getElementById('BusId').attributes['value'];
+      _temp['cancelKey'] =
+          _input_document.getElementById('ReserveId').attributes['value'];
+      _temp['isReserve'] =
+          (_input_document.getElementById('ReserveStateCode').text == '1');
+
+      var tdElements = trElement.getElementsByTagName('td').sublist(1);
+
+      var format = DateFormat('yyyy/MM/dd HH:mm');
+
+      _temp['departureTime'] = format.parse('$time ${tdElements[0].text}');
+      _temp['reserveCount'] = int.parse(tdElements[1].text);
+      _temp['homeCharteredBus'] = false;
+      _temp['specialTrain'] = '';
+      _temp['description'] = '';
+      _temp['startStation'] = startStation;
+      _temp['endStation'] = endStation;
+      _temp['limitCount'] = 999;
+
+      if (tdElements[2].text != '') {
+        if (tdElements[2].getElementsByTagName('button').isNotEmpty) {
+          var _typeString = tdElements[2]
+              .getElementsByTagName('button')[0]
+              .text
+              .replaceAll(' ', '')
+              .replaceAll('\n', '');
+          if (_typeString == '返鄉專車') {
+            _temp['homeCharteredBus'] = true;
+          }
+          if (_typeString == '試辦專車') {
+            _temp['specialTrain'] = '2';
+          }
+          _temp['description'] = tdElements[2]
+              .getElementsByTagName('button')[0]
+              .attributes['data-content'];
+        }
+      }
+      result.add(_temp);
+    }
+    return result;
+  }
+
+  static ScoreData scores(rawHtml) {
+    final scoreData = ScoreData();
+    final document = html.parse(rawHtml);
+    // generate scores list
+    if (document.getElementById("datatable") == null) {
+      return scoreData;
+    }
+    List<Map<String, dynamic>> scoresList = [];
+    //skip table header
+    var _trElements =
+        document.getElementById("datatable").getElementsByTagName('tr');
+    if (_trElements.length <= 1) {
+      return scoreData;
+    }
+
+    for (var trElement in _trElements.sublist(1)) {
+      // select td element
+      var tdElements = trElement.getElementsByTagName("td");
+
+      if (tdElements.length < 8) {
+        // continue;
+        return scoreData;
+      }
+      scoresList.add({
+        "title": tdElements.elementAt(0).text,
+        "units": tdElements.elementAt(1).text,
+        "hours": tdElements.elementAt(2).text,
+        "required": tdElements.elementAt(3).text,
+        "at": tdElements.elementAt(4).text,
+        "middleScore": tdElements.elementAt(5).text,
+        "semesterScore": tdElements.elementAt(6).text,
+        "remark": tdElements.elementAt(7).text,
+      });
+    }
+
+    //detail data
+    Map<String, dynamic> detailData = {};
+    var detailDiv = document.getElementsByClassName("text-bold text-info");
+    if (detailDiv == null) {
+      return scoreData;
+    }
+    if (detailDiv.length < 4) {
+      return scoreData;
+    }
+    detailData["average"] = detailDiv
+        .elementAt(0)
+        .parent
+        .text
+        .replaceAll(detailDiv.elementAt(0).text, "")
+        .replaceAll("\n", "")
+        .replaceAll(" ", "");
+    detailData["average"] = double.parse(detailData["average"]);
+    detailData["conduct"] = detailDiv
+        .elementAt(1)
+        .parent
+        .text
+        .replaceAll(detailDiv.elementAt(1).text, "")
+        .replaceAll("\n", "")
+        .replaceAll(" ", "");
+    detailData["conduct"] = double.parse(detailData["conduct"]);
+    detailData["classRank"] = detailDiv
+        .elementAt(2)
+        .parent
+        .text
+        .replaceAll(detailDiv.elementAt(2).text, "")
+        .replaceAll("\n", "")
+        .replaceAll(" ", "");
+
+    detailData["departmentRank"] = detailDiv
+        .elementAt(3)
+        .parent
+        .text
+        .replaceAll(detailDiv.elementAt(3).text, "")
+        .replaceAll("\n", "")
+        .replaceAll(" ", "");
+
+    return ScoreData.fromJson({
+      "scores": scoresList,
+      "detail": detailData,
+    });
+  }
+
+  static UserInfo userInfo(rawHtml) {
+    final userInfo = UserInfo();
+    final document = html.parse(rawHtml);
+    final list = document.getElementsByClassName('user-header');
+    if (list.length > 0) {
+      final p = list[0].getElementsByTagName('p');
+      if (p.length >= 2) {
+        userInfo.id = p[0].text.split(' ').first;
+        userInfo.name = p[0].text.split(' ').last;
+        userInfo.department = p[1].text;
+      }
+    }
+    return userInfo;
+  }
+
+  static String getCSRF(rawHtml) {
+    final document = html.parse(rawHtml);
+    for (var inputElement in document.getElementsByTagName("input")) {
+      if (((inputElement.attributes ?? const {})['name'] ?? "") ==
+          "__RequestVerificationToken") {
+        return inputElement.attributes['value'];
+      }
+    }
+    return "";
+  }
+}
