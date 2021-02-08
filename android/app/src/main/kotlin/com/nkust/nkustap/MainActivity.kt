cba8b70dc89e91e49c07989b31c3d5545bc4f9df
RainVisitor <abc873693@gmail.com>
2021-02-08 22:27:15 +0800

    Revert "Use ktx API to transport navigation bar"
    This reverts commit 479e10ea24c3ba386ecc8cecb20264de88a5befc.

--- android/app/src/main/kotlin/com/nkust/nkustap/MainActivity.kt
+++ android/app/src/main/kotlin/com/nkust/nkustap/MainActivity.kt
@@ -1,12 +1,6 @@
 package com.nkust.ap.flutter
 
-import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    override fun onPostResume() {
-        super.onPostResume()
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-        window.navigationBarColor = 0
-    }
 }
