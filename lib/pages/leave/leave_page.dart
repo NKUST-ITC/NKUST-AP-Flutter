2179858310978fb90f13b215bb6354ed5c3b82bc
RainVisitor <abc873693@gmail.com>
2021-02-28 23:51:25 +0800

    Improve leave page login behaviar

--- lib/pages/leave/leave_page.dart
+++ lib/pages/leave/leave_page.dart
@@ -1,8 +1,13 @@
 import 'package:ap_common/resources/ap_icon.dart';
 import 'package:ap_common/resources/ap_theme.dart';
 import 'package:ap_common/utils/ap_localizations.dart';
+import 'package:ap_common/utils/preferences.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:nkust_ap/api/helper.dart';
+import 'package:nkust_ap/api/mobile_nkust_helper.dart';
+import 'package:nkust_ap/config/constants.dart';
+import 'package:nkust_ap/models/mobile_cookies_data.dart';
 import 'package:nkust_ap/pages/leave/leave_apply_page.dart';
 import 'package:nkust_ap/pages/leave/leave_record_page.dart';
 
@@ -30,6 +35,8 @@ class LeavePageState extends State<LeavePage>
 
   InAppWebViewController webViewController;
 
+  Future<bool> _init;
+
   String get path {
     switch (_currentIndex) {
       case 0:
@@ -46,6 +53,7 @@ class LeavePageState extends State<LeavePage>
     _currentIndex = widget.initIndex;
     controller =
         TabController(length: 2, initialIndex: widget.initIndex, vsync: this);
+    _init = Future.microtask(() => _loadData());
   }
 
   @override
@@ -62,13 +70,35 @@ class LeavePageState extends State<LeavePage>
         title: Text(ap.leave),
         backgroundColor: ApTheme.of(context).blue,
       ),
-      body: InAppWebView(
-        initialUrl: path,
-        initialOptions: InAppWebViewGroupOptions(
-          crossPlatform: InAppWebViewOptions(),
-        ),
-        onWebViewCreated: (InAppWebViewController webViewController) {
-          this.webViewController = webViewController;
+      body: FutureBuilder<bool>(
+        future: _init,
+        builder: (context, snapshot) {
+          return InAppWebView(
+            initialUrl: path,
+            initialOptions: InAppWebViewGroupOptions(
+              crossPlatform: InAppWebViewOptions(),
+            ),
+            onWebViewCreated: (InAppWebViewController webViewController) {
+              this.webViewController = webViewController;
+            },
+            onPageCommitVisible: (controller, title) async {
+              final path = await controller.getUrl();
+              debugPrint('onPageCommitVisible $title $path');
+              if (path.contains(MobileNkustHelper.LOGIN)) {
+                await webViewController.evaluateJavascript(
+                    source:
+                        'document.getElementsByName("Account")[0].value = "${Helper.username}";');
+                await webViewController.evaluateJavascript(
+                    source:
+                        'document.getElementsByName("Password")[0].value = "${Helper.password}";');
+                await webViewController.evaluateJavascript(
+                    source:
+                        'document.getElementsByName("RememberMe")[0].checked = true;');
+              } else {
+                _finishLogin();
+              }
+            },
+          );
         },
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -95,5 +125,49 @@ class LeavePageState extends State<LeavePage>
       controller.animateTo(_currentIndex);
       webViewController.loadUrl(url: path);
     });
+  }
+
+  Future<bool> _loadData() async {
+    final cookieManager = CookieManager.instance();
+    final cookiesData = MobileNkustHelper.instance.cookiesData;
+    if (cookiesData != null) {
+      for (var i = 0; i < cookiesData.cookies.length; i++) {
+        final element = cookiesData.cookies[i];
+        await cookieManager.setCookie(
+          url: MobileNkustHelper.BASE_URL,
+          name: element.name,
+          value: element.value,
+        );
+      }
+    }
+    return true;
+  }
+
+  Future<void> _finishLogin() async {
+    final cookies = await CookieManager.instance()
+        .getCookies(url: MobileNkustHelper.BASE_URL);
+    final data = MobileCookiesData(cookies: []);
+    cookies.forEach(
+      (element) {
+        data.cookies.add(
+          MobileCookies(
+            path: MobileNkustHelper.HOME,
+            name: element.name,
+            value: element.value,
+            domain: element.domain ??
+                (element.name == '.AspNetCore.Cookies'
+                    ? 'mobile.nkust.edu.tw'
+                    : '.nkust.edu.tw'),
+          ),
+        );
+      },
+    );
+    MobileNkustHelper.instance.setCookieFromData(data);
+    data.save();
+    Preferences.setInt(
+      Constants.MOBILE_COOKIES_LAST_TIME,
+      DateTime.now().microsecondsSinceEpoch,
+    );
+    Navigator.pop(context, true);
   }
 }
