13641bee559c16cf189f0ac7d0a7320f41c42ce2
RainVisitor <abc873693@gmail.com>
2021-03-02 00:05:32 +0800

    Remove old version migrate

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,6 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:ap_common/models/course_data.dart';
-import 'package:ap_common/models/score_data.dart';
-import 'package:ap_common/models/user_info.dart';
-import 'package:ap_common/resources/ap_theme.dart';
 import 'package:ap_common/utils/preferences.dart';
 import 'package:ap_common_firebase/utils/firebase_utils.dart';
 import 'package:google_sign_in_dartio/google_sign_in_dartio.dart';
@@ -36,7 +33,6 @@ void main() async {
   );
   var currentVersion =
       Preferences.getString(Constants.PREF_CURRENT_VERSION, '0');
-  if (int.parse(currentVersion) < 30400) _preference340Migrate();
   if (int.parse(currentVersion) < 30603) CourseData.migrateFrom0_10();
   if (!kIsWeb && (Platform.isMacOS || Platform.isWindows || Platform.isLinux))
     GoogleSignInDart.register(
@@ -53,52 +49,4 @@ void main() async {
     });
   } else
     runApp(MyApp());
-}
-
-//v3.4.0 preference migrate
-void _preference340Migrate() async {
-  String themeCode = Preferences.getString(Constants.PREF_THEME_CODE, null);
-  if (themeCode != null) {
-    int index;
-    switch (themeCode) {
-      case ApTheme.DARK:
-        index = 2;
-        break;
-      case ApTheme.LIGHT:
-        index = 1;
-        break;
-      default:
-        index = 0;
-        break;
-    }
-    await Preferences.setInt(Constants.PREF_THEME_MODE_INDEX, index);
-    Preferences.setString(Constants.PREF_THEME_CODE, null);
-  }
-  Preferences.prefs.getKeys()?.forEach((key) {
-    if (key.contains(Constants.PREF_COURSE_DATA)) {
-      debugPrint(key);
-      var data = Preferences.getString(key, null);
-      var courseData = CourseData.fromRawJson(data);
-      courseData.save(
-        key.replaceAll('${Constants.PREF_COURSE_DATA}_', ''),
-      );
-      Preferences.setString(key, null);
-    } else if (key.contains(Constants.PREF_SCORE_DATA)) {
-      print(key);
-      var data = Preferences.getString(key, null);
-      var courseData = ScoreData.fromRawJson(data);
-      courseData.save(
-        key.replaceAll('${Constants.PREF_SCORE_DATA}_', ''),
-      );
-      Preferences.setString(key, null);
-    } else if (key.contains(Constants.PREF_USER_INFO)) {
-      print(key);
-      var data = Preferences.getString(key, null);
-      var courseData = UserInfo.fromRawJson(data);
-      courseData.save(
-        key.replaceAll('${Constants.PREF_USER_INFO}_', ''),
-      );
-      Preferences.setString(key, null);
-    }
-  });
 }
