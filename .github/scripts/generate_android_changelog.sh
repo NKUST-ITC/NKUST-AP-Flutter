2657bf612e46ebf7162b5a233540da7a4cfaec63
RainVisitor <abc873693@gmail.com>
2021-03-04 23:32:07 +0800

    Fix android generate changelogs path

--- .github/scripts/generate_android_changelog.sh
+++ .github/scripts/generate_android_changelog.sh
@@ -4,8 +4,8 @@ declare -a locales=("en-US" "zh-TW")
 
 for locale in "${locales[@]}"
 do
-  mkdir -p android/fastlane/metadata/android/"${locale}"/
-  jq -r ".\"$1\".\"${locale}\"" assets/changelog.json >> android/fastlane/metadata/android/"${locale}"/default.txt
+  mkdir -p android/fastlane/metadata/android/"${locale}"/changelogs/
+  jq -r ".\"$1\".\"${locale}\"" assets/changelog.json >> android/fastlane/metadata/android/"${locale}"/changelogs/default.txt
 done
 
 echo "Generate android changelog success"
