903c7c326dbc70478b99610d3f0fe0a0e8c32a3f
RainVisitor <abc873693@gmail.com>
2021-03-04 19:55:18 +0800

    Add iOS auto generate changelog

---
+++ .github/scripts/generate_ios_changelog.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+declare -a locales=("en-US" "zh-TW")
+
+for locale in "${locales[@]}"
+do
+  jq -r ".\"$1\".\"${locale}\"" ../assets/changelog.json >> fastlane/changelog.txt
+done
+
+echo "Generate iOS changelog success"
