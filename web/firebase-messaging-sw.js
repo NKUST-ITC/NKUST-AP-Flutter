cf7930b008838d246cc84931020a53b7eff1adc8
Rainvisitor <abc873693@gmail.com>
2021-12-28 15:02:40 +0800

    Update web firebase version

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/8.6.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/8.6.1/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
 
 firebase.initializeApp( {
     apiKey: "AIzaSyBpHUWczANDziA-Gzxoh_ybGnEZq9JmKL4",
