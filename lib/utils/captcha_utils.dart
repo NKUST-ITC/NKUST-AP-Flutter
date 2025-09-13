140f47b815ca8f486e34c6f08beefbad5ccc57a1
LSweetSour <61788097+yappy2000d@users.noreply.github.com>
2025-09-13 22:23:56 +0800

    feat: add Euclidean distance-based captcha solver and related assets

--- lib/utils/captcha_utils.dart
+++ lib/utils/captcha_utils.dart
@@ -5,51 +5,12 @@ import 'dart:typed_data';
 import 'package:image/image.dart' as img;
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:tflite_flutter/tflite_flutter.dart';
-
-const List<String> labels = <String>[
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z',
-  '0',
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-];
+import 'eucdist.dart' as eucdist;
 
 class CaptchaUtils {
   CaptchaUtils._();
 
-  static Future<String> extractByTfLite({
+  static Future<String> extractByEucDist({
     required Uint8List bodyBytes,
   }) async {
     const int digitsCount = 4;
@@ -66,50 +27,17 @@ class CaptchaUtils {
       DateTime end = DateTime.now();
       final img.Image source =
           img.decodeImage(File(imagePath).readAsBytesSync())!;
-      final img.Image grayscaleImage = img.grayscale(source);
-      start = DateTime.now();
-      final Interpreter interpreter = await Interpreter.fromAsset(
-        'assets/webap_captcha.tflite',
-      );
-      end = DateTime.now();
-      final int loadModelTime =
-          end.millisecondsSinceEpoch - start.millisecondsSinceEpoch;
-      log('loadModel time = $loadModelTime ms');
-      final StringBuffer replaceText = StringBuffer();
-      start = DateTime.now();
-      const int w = imageWidth ~/ digitsCount;
-      const int h = imageHeight;
-      for (int i = 0; i < digitsCount; i++) {
-        final img.Image target = img.copyCrop(
-          grayscaleImage,
-          x: (imageWidth ~/ digitsCount) * i,
-          y: 0,
-          width: w,
-          height: h,
-        );
-        final List<dynamic> output =
-            List<dynamic>.filled(1 * labels.length, 0).reshape(<int>[
-          1,
-          labels.length,
-        ]);
-        interpreter.run(
-          imageToByteListFloat32(target, w, h, 127.5, 255.0),
-          output,
-        );
-        if (output.first case final List<double> list?) {
-          final List<double> flattedOutputs = list.toList();
-          flattedOutputs.sort();
-          final int maxIndex = list.indexOf(flattedOutputs.last);
-          replaceText.write(labels[maxIndex]);
-        }
-      }
+      
+
+      final String result = await eucdist.solveByEucDist(source);
+
+
       end = DateTime.now();
       final int processTime =
           end.millisecondsSinceEpoch - start.millisecondsSinceEpoch;
       log('process time = $processTime ms');
-      log(replaceText.toString());
-      interpreter.close();
-      return replaceText.toString();
+      log(result);
+      return result;
     } catch (_) {
       rethrow;
     }
