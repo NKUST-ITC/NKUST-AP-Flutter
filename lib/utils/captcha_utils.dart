8dadcef1eca6dd2be076637810c0b17b96500928
LSweetSour <61788097+yappy2000d@users.noreply.github.com>
2025-09-15 03:25:08 +0800

    refactor: Avoid tedious image reading and writing

--- lib/utils/captcha_utils.dart
+++ lib/utils/captcha_utils.dart
@@ -1,11 +1,11 @@
 import 'dart:developer';
-import 'dart:io';
+// import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:image/image.dart' as img;
 import 'package:nkust_ap/utils/eucdist.dart' as eucdist;
-import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
+// import 'package:path/path.dart';
+// import 'package:path_provider/path_provider.dart';
 
 class CaptchaUtils {
   CaptchaUtils._();
@@ -14,15 +14,20 @@ class CaptchaUtils {
     required Uint8List bodyBytes,
   }) async {
     try {
-      final Directory directory = await getTemporaryDirectory();
-      final String imagePath = join(
-        directory.path,
-        'tmp.jpg',
-      );
-      await File(imagePath).writeAsBytes(bodyBytes);
+      // final Directory directory = await getTemporaryDirectory();
+      // final String imagePath = join(
+      //   directory.path,
+      //   'tmp.jpg',
+      // );
+      // await File(imagePath).writeAsBytes(bodyBytes);
       final DateTime start = DateTime.now();
-      final img.Image source =
-          img.decodeImage(File(imagePath).readAsBytesSync())!;
+      // final img.Image source =
+      //     img.decodeImage(File(imagePath).readAsBytesSync())!;
+      final img.Image? source = img.decodeJpg(bodyBytes);
+
+      if (source == null) {
+        throw Exception('Failed to decode verification code image.');
+      }
 
       final String result = await eucdist.solveByEucDist(source);
 
