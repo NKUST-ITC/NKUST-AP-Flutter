4b787a0d37170bad7d70e189e3bf09714fd005fa
Rainvisitor <abc873693@gmail.com>
2026-09-04 01:41:34 +0800

    feat(login): embed the turnstile challenge in the student id form

--- lib/l10n/generated/strings.g.dart
+++ lib/l10n/generated/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 3
 /// Strings: 627 (209 per locale)
 ///
-/// Built on 2026-09-03 at 09:30 UTC
+/// Built on 2026-09-03 at 17:31 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -25,6 +25,7 @@ import shared_preferences_foundation
 import sign_in_with_apple
 import sqflite_darwin
 import url_launcher_macos
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSaverPlugin.register(with: registry.registrar(forPlugin: "FileSaverPlugin"))
@@ -47,4 +48,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
